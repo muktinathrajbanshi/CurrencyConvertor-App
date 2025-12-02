2e59bed52e7bb6d86832fe8a6ccd0354a8a98097
unknown <muktinathrajbanshi786@gmail.com>
2025-12-02 15:13:23 +0530

    wow it's amazing

--- src/App.jsx
+++ src/App.jsx
@@ -17,12 +17,31 @@ const App = () => {
             <div>
               <label htmlFor="">
                 From:
-                <select name="" id="">
-                  <option value="USD"></option>
+                <select>
+                  <option value="USD">USD</option>
+                  <option value="EUR">EUR</option>
+                  <option value="INR">INR</option>
+                  <option value="NEP">NEP</option>
+                  <option value="AUD">AUD</option>
+                </select>
+              </label>
+            </div>
+
+            <div>
+              <label htmlFor="">
+                To:
+                <select>
+                  <option value="NEP">NEP</option>
+                  <option value="USD">USD</option>
+                  <option value="EUR">EUR</option>
+                  <option value="INR">INR</option>
+                  <option value="AUD">AUD</option>
                 </select>
               </label>
             </div>
           </div>
+
+          
         </div>
       </section>
   );
