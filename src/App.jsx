39812d1c4a4e260aa1a532dfc9064b042c547783
unknown <muktinathrajbanshi786@gmail.com>
2025-12-02 15:09:44 +0530

    Wow what an idea for creating structure with design

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,21 @@ const App = () => {
         <div className="currency-div">
           <h1>Currency Converter</h1>
           <div>
-            <label htmlFor="currency_amount"></label>
+            <label htmlFor="currency_amount">
+            Amount:
+            <input type="number" id="currency_amount" />
+            </label>
+          </div>
+
+          <div className="currency-selector">
+            <div>
+              <label htmlFor="">
+                From:
+                <select name="" id="">
+                  <option value="USD"></option>
+                </select>
+              </label>
+            </div>
           </div>
         </div>
       </section>
