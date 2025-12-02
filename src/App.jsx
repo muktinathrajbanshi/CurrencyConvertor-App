846b1139c17801cdb99839562518341de891a5df
unknown <muktinathrajbanshi786@gmail.com>
2025-12-02 15:04:16 +0530

    wow let's focus on structure of this project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const App = () => {
-  return <h1>Welcome to the Currency Converter App!</h1>
+
+  return (
+      <section className="currency-converter">
+        <div className="currency-div">
+          <h1>Currency Converter</h1>
+          <div>
+            <label htmlFor="currency_amount"></label>
+          </div>
+        </div>
+      </section>
+  );
 };
 
 export default App;
