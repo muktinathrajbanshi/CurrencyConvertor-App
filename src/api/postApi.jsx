cdf25b5a02756b6fbcaa1f8980957dd7f0b7c70a
unknown <muktinathrajbanshi786@gmail.com>
2025-12-02 14:38:12 +0530

    let's get started

---
+++ src/api/postApi.jsx
@@ -0,0 +1,8 @@
+
+// https://v6.exchangerate-api.com/v6/c0e4651455bb1b1672349f0d/latest/USD
+
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "https://v6.exchangerate-api.com/v6/c0e4651455bb1b1672349f0d/latest/USD",
+})
